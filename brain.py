b9da036aee8bbaf6ed00afb45dd12e734345305b
Joel Bryla <joel@wiredlizard.com>
2018-03-12 21:29:31 +1100

    sets default score

--- brain.py
+++ brain.py
@@ -1,6 +1,7 @@
 from elo import El
 from data import Store
-
+from tqdm import tqdm
+from pprint import pprint
 
 class Brain:
     el = El(32)
@@ -10,20 +11,25 @@ class Brain:
 
     predictions = {}
 
-    def __init__(self):
+    def __init__(self):       
+        self.default_score = 0
         self.run_past_matches()
 
     def run_past_matches(self):
-        for match in self.store.matches:
+        for match in tqdm(self.store.matches):
             red_alliance = match["red_alliance"]
             blue_alliance = match["blue_alliance"]
+
+            for team in red_alliance + blue_alliance:
+                if team not in self.scores.keys():
+                    self.scores[team] = self.default_score
 
             prediction = self.predict(red_alliance, blue_alliance, key=match["key"])
             self.update_score(red_alliance, blue_alliance, prediction, match["score"])
 
     def predict(self, red_alliance, blue_alliance, key=False):
         red_score = sum([self.scores[team] for team in red_alliance]) / 3
-        blue_score = sum([self.rankings[team] for team in blue_alliance]) / 3
+        blue_score = sum([self.scores[team] for team in blue_alliance]) / 3
 
         score = self.el.predict(red_score, blue_score)
 
@@ -38,3 +44,8 @@ class Brain:
 
         for team in blue_alliance:
             self.scores[team] = self.el.update(self.scores[team], 1 - prediction, score)
+
+if __name__ == "__main__":
+    a = Brain()
+    pprint(a.scores)
+    print(a.predict(["frc4817", "frc3008", "frc4802"], ["frc6836", "frc6083", "frc4253"]))
