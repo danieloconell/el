9b430feeaeff09b554b82dd9d13dedc8f464bce4
Joel Bryla <joel@wiredlizard.com>
2018-03-17 09:11:47 +1100

    added brier score

--- test.py
+++ test.py
@@ -4,17 +4,20 @@ brain = Brain()
 
 results = []
 results1 = []
-
-for match in brain.store.years[2017]:
+print()
+for match in brain.store.matches[2018]:
     a = brain.predict(match["red_alliance"], match["blue_alliance"])
-    if a >= 0.5 and match["score"] >= 0.5:
+
+    score = brain.get_score(match["red_score"], match["blue_score"])
+
+    if a >= 0.5 and score >= 0.5:
         results.append(True)
-    elif a < 0.5 and match["score"] < 0.5:
+    elif a < 0.5 and score < 0.5:
         results.append(True)
     else:
         results.append(False)
 
-    results1.append((match["score"] - a)** 2 )
+    results1.append((score - a) ** 2)
 
 print(str(round(results.count(True) / len(results)*100, 2)) + "%")
 print("Brier:")
